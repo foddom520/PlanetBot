66ba94f2ab1602e53d353d6fc8677bfe54837120
foddom520 <115153461+foddom520@users.noreply.github.com>
2024-09-18 19:45:05 +0200

    v2

--- Cogs/test.py
+++ Cogs/test.py
@@ -2,12 +2,16 @@ import discord
 from discord import app_commands
 import math
 from discord.ext import commands
+import datetime
+import pytesseract
+import cv2
+import numpy as np
 
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command( )
+    @commands.command()
     async def ping(self, ctx):
         ping_embed = discord.Embed(title="Ping", color=discord.Color.blue())
         ping_embed.add_field(name=f"{self.bot.user.name}'s Latency (ms): ", value=f"{round(self.bot.latency * 1000)}ms", inline=False)
@@ -16,46 +20,33 @@ class Test(commands.Cog):
 
     @app_commands.command(name="fl_run_calculator", description="Will tell you the total exp, runs required and estimated time to go from one level to the next")
     async def fl_run_calculator(self, interaction: discord.Interaction, start_level: int, end_level: int, exp_per_run: str, current_exp: str, run_time_mins: float = None):
+        if end_level <= start_level:
+            return await interaction.response.send_message("Error: End level must be at least 1 higher than the start level.")
+    
         def calculate_exp(start_level, end_level):
-            total_exp = 0
-            for level in range(start_level, end_level):
-                exp = 50 * (1.15 ** level)
-                total_exp += exp
-            return total_exp
+            return sum(50 * (1.15 ** level) for level in range(start_level, end_level))
 
         def convert_to_number(s):
             s = s.lower()
-            if s.endswith('k'):
-                return int(float(s[:-1]) * 10**3)
-            elif s.endswith('m'):
-                return int(float(s[:-1]) * 10**6)
-            elif s.endswith('b'):
-                return int(float(s[:-1]) * 10**9)
-            elif s.endswith('t'):
-                return int(float(s[:-1]) * 10**12)
-            else:
-                return int(s)
+            suffixes = {'k': 10**3, 'm': 10**6, 'b': 10**9, 't': 10**12, 'q': 10**15, 'qi': 10**18, 'sx': 10**21, 'sp': 10**24, 'oc': 10**27, 'n': 10**30, 'dc': 10**33, 'ud': 10**36, 'dd': 10**39, 'td': 10**42, 'qd': 10**45, 'qid': 10**48, 'sd': 10**51, 'spd': 10**54, 'ocd': 10**57, 'nd': 10**60, 'vg': 10**63}
+            for suffix, value in suffixes.items():
+                if s.endswith(suffix):
+                    return int(float(s[:-1]) * value)
+            return int(s)
 
         def format_number(num):
-            if num >= 1000000000000:
-                return f"{num / 1000000000000:.2f}t"
-            elif num >= 1000000000:
-                return f"{num / 1000000000:.2f}b"
-            elif num >= 1000000:
-                return f"{num / 1000000:.2f}m"
-            elif num >= 1000:
-                return f"{num / 1000:.2f}k"
-            else:
-                return str(num)
+            suffixes = ['Vg', 'Nd', 'Ocd', 'Spd', 'Sd', 'QiD', 'Qd', 'Td', 'Dd', 'Ud', 'Dc', 'N', 'Oc', 'Sp', 'Sx', 'Qi', 'Q', 'T', 'B', 'M', 'K']
+            values = [10**63, 10**60, 10**57, 10**54, 10**51, 10**48, 10**45, 10**42, 10**39, 10**36, 10**33, 10**30, 10**27, 10**24, 10**21, 10**18, 10**15, 10**12, 10**9, 10**6, 10**3]
+            for suffix, value in zip(suffixes, values):
+                if num >= value:
+                    return f"{num / value:.2f}{suffix}"
+            return str(num)
 
         def calculate_runs(total_exp, exp_per_run):
-            runs_needed = total_exp / exp_per_run
-            return runs_needed
+            return total_exp / exp_per_run
 
         def calculate_time(runs_needed, run_time_mins):
-            total_time_mins = runs_needed * run_time_mins
-            total_time_hours = total_time_mins / 60
-            return total_time_hours
+            return runs_needed * run_time_mins / 60
 
         total_exp = calculate_exp(start_level, end_level)
         current_exp = convert_to_number(current_exp)
@@ -64,31 +55,167 @@ class Test(commands.Cog):
         runs_needed = calculate_runs(total_exp, exp_per_run)
 
         embed = discord.Embed(title="FL Run Calculator Results", color=0x280137)
-        embed.set_thumbnail(url="https://i.imgur.com/4MkIkzt.png")
-        embed.add_field(name="**Leveling Up!**", value=f"From Level {start_level} to Level {end_level}", inline=False)
-        embed.add_field(name="**EXP per Run**", value=f"{format_number(exp_per_run)}", inline=False)
-        embed.add_field(name="**Current EXP**", value=f"{format_number(current_exp)}", inline=False)
+        embed.add_field(name="** **", value=f"Number of runs needed to go from **Level ** **{start_level}** to **Level** **{end_level}** with **{format_number(exp_per_run)}** experience per run.", inline=False)
         embed.add_field(name="**Total EXP Required**", value=f"{format_number(total_exp)}", inline=False)
-        embed.add_field(name="**Runs Required**", value=f"{math.ceil(runs_needed)}", inline=False)
+        embed.add_field(name="**Runs Required**", value=f"{format_number(math.ceil(runs_needed))}", inline=False)
         if run_time_mins is not None:
             embed.add_field(name="**Run Time (minutes)**", value=f"{run_time_mins:.2f}", inline=False)
             total_time_hours = calculate_time(runs_needed, run_time_mins)
             embed.add_field(name="**Estimated Time**", value=f"{total_time_hours:.2f} hours", inline=False)
-        embed.set_footer(text="Good luck with your leveling!")
+        embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text=f"{interaction.guild.name} |  Annarex", icon_url=interaction.guild.icon.url if interaction.guild.icon else None)
 
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(name="help", description="Get help on how to use the FL Run Calculator command")
-    async def help(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="FL Run Calculator Help", color=0x280137)
-        embed.add_field(name="Command Syntax", value="/fl_run_calculator <start_level> <end_level> <exp_per_run> [run_time_mins]", inline=False)
-        embed.add_field(name="Arguments:", value="*", inline=False)
-        embed.add_field(name="  `<start_level>`", value="The current level you are at. (integer)", inline=False)
-        embed.add_field(name="  `<end_level>`", value="The level you want to reach. (integer)", inline=False)
-        embed.add_field(name="  `<exp_per_run>`", value="The amount of EXP you gain per run. You can use suffixes like 'k', 'm', 'b', or 't' to represent thousands, millions, billions, or trillions, respectively. (string)", inline=True)
-        embed.add_field(name="  `[run_time_mins]`", value="The time it takes to complete one run in minutes. (float, optional) (for example 4.10 means 4 minutes .10 means 10 seconds)", inline=False)
-        embed.add_field(name="Example:", value="/fl_run_calculator 10 20 5k 10.5", inline=False)
-        embed.set_footer(text="If you have any questions or need further assistance, feel free to ask!")
+    @app_commands.command(name="pot_calculator", description="Calculate the pot value")
+    async def pot_calculator(self, interaction: discord.Interaction, base_pot: int, num_upgrades: int, upgrades_done: int = 0):
+        result = base_pot * (1.0050004482 ** (num_upgrades - upgrades_done))
+        embed = discord.Embed(title="Potential Calculator", color=0x280137)
+        embed.add_field(name="**Max Power**", value=f"{math.ceil(result)}", inline=False)
+        embed.add_field(name="**Base Potential**", value=f"{base_pot:.2f}", inline=False)
+        embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text=f"{interaction.guild.name} |  Annarex", icon_url=interaction.guild.icon.url if interaction.guild.icon else None)
+        await interaction.response.send_message(embed=embed)
+
+    
+    @app_commands.command(name="ring_calculator", description="Calculate the rings pot")
+    async def ring_calculator(self, interaction: discord.Interaction, base_pot: int, num_runs: int, runs_done: int = 0):
+        result = base_pot * (1.01 ** (num_runs - runs_done))
+        embed = discord.Embed(title="Ring Pot Calculator Result", color=0x280137)
+        embed.add_field(name="Base Pot", value=f"{base_pot:.2f}", inline=False)
+        embed.add_field(name="Number of Runs", value=num_runs, inline=False)
+        embed.add_field(name="Runs Done", value=runs_done, inline=False)
+        embed.add_field(name="Calculated Pot Value", value=f"{math.ceil(result)}", inline=False)
+        embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text=f"{interaction.guild.name} |  Annarex", icon_url=interaction.guild.icon.url if interaction.guild.icon else None)
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="reroll_calculator", description="Calculate reroll probabilities")
+    @app_commands.describe(maps="Choose a map.")
+    @app_commands.choices(maps=[
+        discord.app_commands.Choice(name="EF",  value="EF"),
+        discord.app_commands.Choice(name="SC",  value="SC"),
+    ])
+    @app_commands.describe(item="Choose an item type.")
+    @app_commands.choices(item=[
+        discord.app_commands.Choice(name="weapon",  value="weapon"),
+        discord.app_commands.Choice(name="armor",  value="armor"),
+    ])
+    @app_commands.describe(classes="Choose a class.")
+    @app_commands.choices(classes=[
+        discord.app_commands.Choice(name="War/mage",  value="war/mage"),
+        discord.app_commands.Choice(name="Guardian",  value="guardian"),
+    ])
+    @app_commands.describe(rarity="Choose a rarity.")
+    @app_commands.choices(rarity=[
+        discord.app_commands.Choice(name="Secret",  value="secret"),
+        discord.app_commands.Choice(name="Legendary",  value="legendary"),
+        discord.app_commands.Choice(name="Epic",  value="epic"),
+    ])
+    async def reroll_calculator(self, interaction: discord.Interaction, item: str, current_value: float, maps: discord.app_commands.Choice[str], classes: discord.app_commands.Choice[str], rarity: discord.app_commands.Choice[str]):
+        try:
+            ranges = {
+    "EF": {
+        "war/mage": {
+            "secret": {"weapon": (1527126, 1730499)},
+            "legendary": {"weapon": (1324010, 1500035), "armor": (1141963, 1294038)},
+            "epic": {"armor": (1016980, 1152418)},
+        },
+        "guardian": {
+            "legendary": {"armor": (22066616, 25005283)},
+        },
+    },
+    "SC": {
+        "war/mage": {
+            "secret": {"weapon": (509055, 576849)},
+            "legendary": {"weapon": (441348, 500123)},
+            "secret": {"armor": (475163, 525859)},
+            "legendary": {"armor": (380663, 431359)},
+            "epic": {"armor": (339002, 384153)},
+        },
+        "guardian": {
+            "secret": {"armor": (10723329, 11751857)},
+            "legendary": {"armor": (7723329, 8751857)},
+        },
+    },
+}
+
+            if maps.value not in ranges:
+                await interaction.response.send_message(f"Invalid map: {maps.value}")
+                return
+
+            if classes.value not in ranges[maps.value]:
+                await interaction.response.send_message(f"Invalid class: {classes.value} on map {maps.value}")
+                return
+
+            if rarity.value not in ranges[maps.value][classes.value]:
+                await interaction.response.send_message(f"Invalid rarity: {rarity.value} on map {maps.value} for class {classes.value}")
+                return
+
+            minimum_value, maximum_value = ranges[maps.value][classes.value][rarity.value].get(item, (None, None))
+
+            if minimum_value is not None and maximum_value is not None:
+                if current_value < minimum_value or current_value > maximum_value:
+                    await interaction.response.send_message(f"Incorrect item pot: {item} on map {maps.value} for class {classes.value} with rarity {rarity.value}. Value must be between {minimum_value} and {maximum_value}.")
+                    return
+            
+            if classes.value == "guardian" and item == "weapon":
+                await interaction.response.send_message("Hey, you can't match a weapon with the Guardian class! There is no such thing.")
+                return
+            
+            if minimum_value is None or maximum_value is None:
+                await interaction.response.send_message(f"Invalid item type: {item} on map {maps.value} for class {classes.value} with rarity {rarity.value}")
+                return
+
+            probability_up = (maximum_value - current_value) / (maximum_value - minimum_value)
+            probability_down = (current_value - minimum_value) / (maximum_value - minimum_value)
+
+            embed = discord.Embed(title="Probability Calculation", color=0x280137)
+            embed.add_field(name="Item Information", value=f"Map: {maps.value}\nClass: {classes.value}\nRarity: {rarity.value}\nItem Type: {item}\nOriginal Pot: {int(current_value)}", inline=False)
+            embed.add_field(name="Probability Up", value=f"{int(probability_up * 100)}%", inline=False)
+            embed.add_field(name="Probability Down", value=f"{int(probability_down * 100)}%", inline=False)
+            embed.timestamp = datetime.datetime.now()
+            embed.set_footer(text=f"{interaction.guild.name} |  Annarex", icon_url=interaction.guild.icon.url if interaction.guild.icon else None)
+
+            await interaction.response.send_message(embed=embed)
+        except Exception as e:
+            await interaction.response.send_message(f"An error occurred: {e}")
+
+    
+    @app_commands.command(name="image_reader", description="Read numbers from an image")
+    async def image_reader(self, interaction: discord.Interaction, image: discord.Attachment):
+        if not image.content_type.startswith("image/"):
+            await interaction.response.send_message("Please attach an image.")
+            return
+        
+        image_data = await image.read()
+
+        img = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        text = pytesseract.image_to_string(gray)
+
+        lines = text.splitlines()
+
+        lines = [line for line in lines if line.strip()]
+
+        if len(lines) not in [6, 7]:
+            await interaction.response.send_message("Invalid image. Please attach an image with either 6 or 7 lines of numbers.")
+            return
+
+        second_line = lines[1]
+        third_line = lines[2]
+
+        higher_line = max(second_line, third_line, key=lambda x: float(x.replace(',', '')))
+
+        second_last_line = lines[-2]
+
+        embed = discord.Embed(title="Extracted Numbers", color=0x280137)
+        embed.add_field(name="Higher Line", value=higher_line, inline=False)
+        embed.add_field(name="Second Last Line", value=second_last_line, inline=False)
+        embed.timestamp = datetime.datetime.now()
+        embed.set_footer(text=f"{interaction.guild.name} |  Annarex", icon_url=interaction.guild.icon.url if interaction.guild.icon else None)
 
         await interaction.response.send_message(embed=embed)
 
--- main.py
+++ main.py
@@ -3,30 +3,48 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import asyncio
-import pyautoreload
+import logging
+
+logging.basicConfig(level=logging.ERROR)
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+logger = logging.getLogger("bot")
+
+@bot.event
+async def on_connect():
+    print("Bot connected to discord!")
+    await sync_commands()
 
 @bot.event
 async def on_ready():
     print("Bot ready!")
+
+async def sync_commands():
     try:
         synced_commands = await bot.tree.sync()
-        print(f"Synced {len(synced_commands)} commands.")
+        print(f"Synced {len(synced_commands)} commands successfully!")
     except Exception as e:
-        print("an error with syncing application commands has occoured: ", e)
+        print(f"Error syncing application commands: {e}")
 
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
+@bot.event
+async def on_command_error(ctx, error):
+    logger.error("Error in command %s: %s", ctx.command, error)
+    await ctx.send(f"Error: {error}")
 
-async def load():
+async def load_cogs():
     for filename in os.listdir("./Cogs"):
         if filename.endswith(".py"):
-            await bot.load_extension(f"Cogs.{filename[:-3]}")
+            try:
+                await bot.load_extension(f"Cogs.{filename[:-3]}")
+            except Exception as e:
+                logger.error("Error loading cog %s: %s", filename, e)
 
 async def main():
     async with bot:
-        await load()
-        await bot.start(TOKEN)
+        await load_cogs()
+        await bot.start(os.getenv("TOKEN"))
 
-asyncio.run(main())
+if __name__ == "__main__":
+    load_dotenv()
+    asyncio.run(main())
